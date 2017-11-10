b06870a9d15aaca3b20dc6ce21d676371800eb61
李茂峰 <limaofeng@msn.com>
2017-11-10 10:24:40 +0800

    集成Router

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 export const connector = require('./connector').default;
 export const redux = require('./redux').default;
 export const apollo = require('./apollo').default;
-export const createClient = require('./apollo').createClient;
+export const router = require('./router').default;
+export const { createClient } = require('./apollo');
